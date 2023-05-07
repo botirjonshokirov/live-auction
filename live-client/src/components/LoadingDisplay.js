a21189c98dc7b9d1a8686ee501303550212dcb5c
Botirjon Shokirov <botirjon@radcode.co>
2023-05-07 23:34:18 +0200

    updated version of site

--- live-client/src/components/LoadingDisplay.js
+++ live-client/src/components/LoadingDisplay.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import spinner from '../images/spinner.gif';
+import React from "react";
+import spinner from "../images/spinner.gif";
 
 const LoadingDisplay = () => {
   return (
     <div>
       <img
         src={spinner}
-        style={{ width: '100px', margin: 'auto', display: 'block' }}
-        alt='Loading...'
+        style={{ width: "100px", margin: "auto", display: "block" }}
+        alt="Loading..."
       />
     </div>
   );
